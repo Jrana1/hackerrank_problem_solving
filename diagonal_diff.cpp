93f859275f03e21363250db107251ad8e028a923
Jrana1 <jewelbutex12@gmail.com>
2022-07-31 09:50:35 +0200

    variable name has been modified

--- diagonal_diff.cpp
+++ diagonal_diff.cpp
@@ -4,30 +4,30 @@ using namespace std;
 // problem link: https://www.hackerrank.com/challenges/diagonal-difference/problem
 int d_d(vector<vector<int>> mat)
 {
-    int sum1 = 0;
+    int sum_d1 = 0;
     for (int i = 0; i < mat.size(); i++)
     {
         for (int j = 0; j < mat.size(); j++)
         {
             if (i == j)
             {
-                sum1 += mat[i][j];
+                sum_d1 += mat[i][j];
             }
         }
     }
     // cout << sum1 << endl;
 
-    int sum2 = 0;
+    int sum_d2 = 0;
     for (int i = 0; i < mat.size(); i++)
     {
         int j;
         for (j = 0; j < mat.size() - i; j++)
         {
         }
-        sum2 += mat[i][j - 1];
+        sum_d2 += mat[i][j - 1];
     }
     // cout << sum2 << endl;
-    return abs(sum1 - sum2);
+    return abs(sum_d1 - sum_d2);
 }
 
 int main()
